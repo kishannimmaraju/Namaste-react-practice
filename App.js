204a2843d89070d3674a41e9faff7805222f2d1d
kishan <kishannimmaraju96@gmail.com>
2024-06-05 16:53:03 +0530

    Episode - 01 | Inception

---
+++ App.js
@@ -0,0 +1,22 @@
+// Nested elements creating in reactjs
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Hello India"),
+    React.createElement("h1", {}, "Hello India"),
+    React.createElement("h1", {}, "Hello India"),
+    React.createElement("h1", {}, "Hello India")
+  )
+);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello Reactjs"
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
